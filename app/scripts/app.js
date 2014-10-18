b03570b779095d5af95b6aca5f82ab1909d50f65
Mike Santo <mike.j.santo@gmail.com>
2014-10-18 00:10:55 -0500

    generated angular app

---
+++ app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name scFriendsApp
+ * @description
+ * # scFriendsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('scFriendsApp', [
+    'ngAnimate',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
